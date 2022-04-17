73d10abc3476d0f368526a5cbcffe002b4359ed0
alex <bokuto@mail.ru>
2022-04-17 16:17:32 +0300

    step 2_5

---
+++ my_page/week_days/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def monday(request):
+    return HttpResponse('Понедельник')
+
+
+def tuesday(request):
+    return HttpResponse('Вторник')
+
+
+def wednesday(request):
+    return HttpResponse('Среда')
+
+
+def thursday(request):
+    return HttpResponse('Четверг')
+
+
+def friday(request):
+    return HttpResponse('Пятница')
+
+
+def saturday(request):
+    return HttpResponse('Суббота')
+
+
+def sunday(request):
+    return HttpResponse('Воскресенье')
